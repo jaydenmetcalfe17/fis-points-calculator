feec516a79b51135c44e6c0c15e7044eec7f1163
jaydenmetcalfe17 <jayden10.9@gmail.com>
2024-10-13 11:58:29 -0400

    minor update before branching

--- penalty_calculation.py
+++ penalty_calculation.py
@@ -11,7 +11,7 @@ from flask import Flask, jsonify, request, render_template
 ## Due to version compatibility issues, ChromeDriver is manually installed since Selenium Manager is not currently working
 
 ##UPDATE CHROME_DRIVER_PATH SPECIFIC TO DEVICE 
-chrome_driver_path = ""
+chrome_driver_path = "/Users/Jayden/Desktop/chromedriver-mac-x64/chromedriver"
 chrome_service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=chrome_service)
 
